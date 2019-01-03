a0cfbf178bf04f5de3427d89d30cda59a9becb11
Daniel Gorter <danieljgorter@gmail.com>
2019-01-03 21:04:06 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -13,6 +13,12 @@ function whileLoop(num){
   return 'done'
 }
 
+var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+}
+ 
 function doWhileLoop(array){
   do {array.pop()} while (array.length > 0 && incrementVariable())
   return array
