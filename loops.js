6353d28686ea44246cfecee03672bc8031b49c69
Daniel Gorter <danieljgorter@gmail.com>
2019-01-03 20:54:37 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,13 @@
+function forLoop(array){
+  for(let i = 0; i < 25; i++){
+    array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`)
+  }
+  return array
+}
+
+function whileLoop(num){
+  while (num >= 0){
+    console.log(num)
+  }
+  return 'done'
+}
